3c0aebbd9b0d000b179b560e82b98459c43690a9
Iain Wilson <iain.iw.wilson@gmail.com>
2013-09-04 11:46:47 +0100

    Call original update action and prevent invalid gift code being redisplayed in form

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -1,26 +1,23 @@
 Spree::OrdersController.class_eval do
 
-  # TODO Apply gift code in a before filter if possible to avoid overriding the update method for easier upgrades?
-  def update
+  def update_with_gift_code
     @order = current_order
-    if @order.update_attributes(params[:order])
-      render :edit and return unless apply_coupon_code if defined?(Spree::Promo)
-      render :edit and return unless apply_gift_code
+    @order.update_attributes(params[:order])
 
-      @order.line_items = @order.line_items.select {|li| li.quantity > 0 }
-      fire_event('spree.order.contents_changed')
-      respond_with(@order) do |format|
-        format.html do
-          if params.has_key?(:checkout)
-            redirect_to checkout_state_path(@order.checkout_steps.first)
-          else
-            redirect_to cart_path
-          end
-        end
+    if @order.gift_code.present?
+      if apply_gift_code
+        @order.line_items = @order.line_items.select {|li| li.quantity > 0 }
+        fire_event('spree.order.contents_changed')
+      else
+        params[:order][:gift_code] = nil
+        @order.gift_code = nil
+        render :edit and return
       end
-    else
-      respond_with(@order)
     end
+
+    update_without_gift_code
   end
+
+  alias_method_chain :update, :gift_code
 
 end
