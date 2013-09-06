465e57263441f683620f459710653b03284353de
Phil <pmcclure@rumblelabs.com>
2013-09-06 13:57:41 +0100

    Custom changes that only apply for my project

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -15,9 +15,33 @@ Spree::OrdersController.class_eval do
       end
     end
 
-    update_without_gift_code
+    update
   end
 
-  alias_method_chain :update, :gift_code
+  def update
+      @order = current_order
+      unless @order
+        flash[:error] = t(:order_not_found)
+        redirect_to root_path and return
+      end
+
+      if @order.update_attributes(params[:order])
+        @order.line_items = @order.line_items.select {|li| li.quantity > 0 }
+        fire_event('spree.order.contents_changed')
+        respond_with(@order) do |format|
+          format.html do
+            if params.has_key?(:checkout)
+              @order.next_transition.run_callbacks if @order.cart?
+              redirect_to checkout_state_path(@order.checkout_steps.first)
+            else
+              redirect_to checkout_state_path(@order.state)
+            end
+          end
+        end
+      else
+        respond_with(@order)
+      end
+    end
+
 
 end
