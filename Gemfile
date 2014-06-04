9d0e65c75b118527a10a217eff525b9c3d3e1cd7
Mark Leetch <markleetch@gmail.com>
2014-06-04 09:23:37 +0100

    lets try base32 crockford for nicer UID's

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source 'http://rubygems.org'
 
 gemspec
+
+gem 'base32-crockford'
--- app/models/spree/gift_card.rb
+++ app/models/spree/gift_card.rb
@@ -61,7 +61,7 @@ module Spree
 
     def generate_code
       until self.code.present? && self.class.where(code: self.code).count == 0
-        self.code = Digest::SHA1.hexdigest([Time.now, rand].join)
+        self.code = Base32::Crockford.encode([Time.now, rand].join)
       end
     end
 
