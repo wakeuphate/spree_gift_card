a1efbe664e1cb9e545b1fba5dfe2480f89c9b576
Toby Osbourn <toby.osbourn@googlemail.com>
2014-12-01 11:18:41 +0000

    Stop sending emails for gift certificates

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -14,9 +14,9 @@ Spree::Order.class_eval do
   def finalize_with_gift_card!
     finalize_without_gift_card!
     # Send out emails for any newly purchased gift cards.
-    self.line_items.each do |li|
-      Spree::OrderMailer.gift_card_email(li.gift_card, self).deliver if li.gift_card
-    end
+    #self.line_items.each do |li|
+    #  Spree::OrderMailer.gift_card_email(li.gift_card, self).deliver if li.gift_card
+    #end
     # Record any gift card redemptions.
     self.adjustments.where(originator_type: 'Spree::GiftCard').each do |adjustment|
       adjustment.originator.debit(adjustment.amount, self)
