65b8758e5d409371147dca4ce8c806edce445aaf
tosbourn <toby.osbourn@googlemail.com>
2014-12-15 10:25:50 +0000

    Hardcode email for admin giftcard notifications
    Emails where coming in from a  generic example address, I could not see where this setting was being made so since this plugin is already marked as for Artful use only I decided to hardcode the value
    
    This closes an issue raised by Nick in an email
    
    t:15

--- app/mailers/spree/order_mailer_decorator.rb
+++ app/mailers/spree/order_mailer_decorator.rb
@@ -5,7 +5,7 @@ Spree::OrderMailer.class_eval do
     subject = "#{Spree::Config[:site_name]} Gift Card"
     @gift_card.update_attribute(:sent_at, Time.now)
     mail(
-      :from => Spree::Config.preferred_emails_sent_from,
+      :from => 'info@theartfulproject.com',
       :to => 'joshua@theartfulproject.com',
       :subject => subject
     )
